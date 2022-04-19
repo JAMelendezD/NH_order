04f48dc36c32be5c9b1c85c31e673078d8af48ac
JAMelendezD <ja.melendez10@uniandes.edu.co>
2022-04-19 10:40:30 -0400

    new features

--- nh_order.py
+++ nh_order.py
@@ -14,9 +14,13 @@ warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
 parser = argparse.ArgumentParser()
 parser.add_argument('top', type=str, help='tpr file')
 parser.add_argument('traj', type=str, help='trajectory file')
+parser.add_argument('first', type=int, help='First frame starts at 0')
+parser.add_argument('last', type=int, help='Last frame inclusive')
 parser.add_argument('sele1', type=str, help='main selection 1')
 parser.add_argument('sele2', type=str, help='main selection 2')
-parser.add_argument('--mode', default=0,required=False, type=int, help='0 acf. 1 iRED')
+parser.add_argument('P', type=int, help='legendre polynomial only options 1 or 2.')
+parser.add_argument('--mode', default=0,required=False, type=int, help='0 acf. 1 acf in blocks. 2 iRED')
+parser.add_argument('--lenacf', default=None,required=False, type=int, help='Length of the ACF maximum frames/2')
 parser.add_argument('out', type=str, help='output file')
 args = parser.parse_args()
 
@@ -32,21 +36,60 @@ def p2_angle(vec1,vec2):
     return(p2)
 
 @jit(nopython=True)
-def acf(vectors,residue,frames):
+def p1_angle(vec1,vec2):
     '''
-    Coputes the autocorrelation function same as GROMACS using P2 to itself
+    Computes the first order legendre polynomial between two vectors
     '''
-    M = int(frames/2)
+    unit_vec1 = vec1 / np.linalg.norm(vec1)
+    unit_vec2 = vec2 / np.linalg.norm(vec2)
+    p1 = np.dot(unit_vec1, unit_vec2)
+    return(p1)
+
+@jit(nopython=True)
+def acf(vectors,residue,frames,length,func):
+    '''
+    Computes the autocorrelation function same as GROMACS using P2 to itself
+    '''
+    if length != None:
+        if length <= int(frames/2):
+            M = length
+        else:
+            M = int(frames/2)
+    else:
+        M = int(frames/2)
+
     C_p = np.zeros(M+1)
     for j in range(M+1):
-        p2_sum = 0
+        p_sum = 0
         for i in range(frames-M):
-            p2_sum += p2_angle(vectors[i][residue],vectors[i+j][residue])
-        C_p[j] = p2_sum/M
+            p_sum += func(vectors[i][residue],vectors[i+j][residue])
+        C_p[j] = p_sum/M
     return(C_p/C_p[0])
 
 @jit(nopython=True)
-def angles(vectors,num_residues,residue,frames):
+def acf_blocks(vectors,residue,frames,length,func):
+    '''
+    Computes the autocorrelation function same as GROMACS using P2 to itself in blocks
+    '''
+    if length != None:
+        if length <= int(frames/2):
+            M = length
+        else:
+            M = int(frames/2)
+    else:
+        M = int(frames/2)
+
+    blocks = int(frames/length)
+    C_p = np.zeros(M+1)
+    for j in range(M+1):
+        p_sum = 0
+        for i in range(blocks-1):
+            p_sum += func(vectors[i*M][residue],vectors[i*M+j][residue])
+        C_p[j] = p_sum/blocks
+    return(C_p/C_p[0])
+
+@jit(nopython=True)
+def angles(vectors,num_residues,residue,frames,func):
     '''
     Computes the correlation to every other residue using P2 needed to generate the matrix for iRED
     '''
@@ -54,7 +97,7 @@ def angles(vectors,num_residues,residue,frames):
     for k in range(num_residues):
         if k >= residue:
             for i in range(frames):
-                angs[k] +=  p2_angle(vectors[i][residue],vectors[i][k])
+                angs[k] +=  func(vectors[i][residue],vectors[i][k])
     return(angs/frames)
 
 def exp_model(x,A0,A1):
@@ -78,7 +121,7 @@ def plot_mat(mat,residues):
     ax.set_yticklabels(x_labels)
     fig = plt.imshow(mat)
     plt.colorbar(fig,fraction=0.046, pad=0.02)
-    plt.savefig(f'{args.out}ired_mat.png',dpi=300,bbox_inches='tight')
+    plt.savefig(f'{args.out}mat_ired_{args.P}.png',dpi=300,bbox_inches='tight')
 
 def ired(mat):
     '''
@@ -107,7 +150,7 @@ def ired(mat):
 
 def run():
     u = MDAnalysis.Universe(args.top,args.traj)
-    len_traj = len(u.trajectory)
+    len_traj = len(u.trajectory[args.first:args.last])
 
     dt = (u.trajectory[1].time-u.trajectory[0].time)*1e-3
 
@@ -130,22 +173,37 @@ def run():
     print(f'The first and last resnums for selection 2:\t{sel2_resnums[0]:5d}{sel2_resnums[-1]:5d}')
 
     vectors = []
-    for ts in tqdm(u.trajectory,colour='green',desc='Frames'):
+    for ts in tqdm(u.trajectory[args.first:args.last],colour='green',desc='Frames'):
         nh_vector = sel1.positions - sel2.positions
         vectors.append(nh_vector)
 
     shape = np.shape(vectors)    
     frames,residues = shape[0],shape[1]
 
-    if args.mode == 0:
-        fig = plt.figure(figsize=(7,6))
-        time = np.arange(int(frames/2)+1)*dt
+    if args.mode == 0 or args.mode == 1:
+        if args.lenacf != None:
+            if args.lenacf <= int(frames/2):
+                time = np.arange(args.lenacf+1)*dt
+            else:
+                time = np.arange(int(frames/2)+1)*dt
+        else:
+            time = np.arange(int(frames/2)+1)*dt
         max_x = max(time)
         min_x = min(time)
         order = []
         residue_ids = []
+        fig = plt.figure(figsize=(7,6))
         for residue in tqdm(range(residues),colour='green',desc='Residues'):
-            C_p = acf(vectors,residue,frames)
+            if args.mode == 0:
+                if args.P == 1:
+                    C_p = acf(vectors,residue,frames,args.lenacf,p1_angle)
+                elif args.P == 2:
+                    C_p = acf(vectors,residue,frames,args.lenacf,p2_angle)
+            elif args.mode == 1:
+                if args.P == 1:
+                    C_p = acf_blocks(vectors,residue,frames,args.lenacf,p1_angle)
+                elif args.P == 2:
+                    C_p = acf_blocks(vectors,residue,frames,args.lenacf,p2_angle)
             result = list(zip(time,C_p))
             np.savetxt(f'{args.out}{sel1[residue].resid}.dat',result,fmt=['%15.3f','%10.5f'],header='{:>13s}{:>11s}'.format('Time (ns)','C_p(t)'))
             fittedParameters, pcov = curve_fit(exp_model, time, C_p, bounds = ([-0.5,min_x],[1,max_x]))
@@ -158,24 +216,27 @@ def run():
         plt.xlim(time[0],time[-1])
         plt.xlabel(r'$\tau$ (ns)')
         plt.ylabel(r'$C$($\tau$)')
-        plt.savefig(f'{args.out}acf.png',dpi=300,bbox_inches='tight')
+        plt.savefig(f'{args.out}acf_{args.mode}_{args.lenacf}_{args.P}.png',dpi=300,bbox_inches='tight')
         result = list(zip(residue_ids,order))
-        np.savetxt(f'{args.out}order_acf.dat',result,fmt=['%8d','%8.5f'],header='{:7s}{:>8s}'.format('Residue','S2'))
+        np.savetxt(f'{args.out}order_acf_{args.mode}_{args.lenacf}_{args.P}.dat',result,fmt=['%8d','%8.5f'],header='{:7s}{:>8s}'.format('Residue','S2'))
 
     
-    elif args.mode == 1:
+    elif args.mode == 2:
         mat = np.zeros((residues,residues))
         residue_ids = []
         for residue in tqdm(range(residues),colour='green',desc='Residues'):
-            mat[:][residue] = angles(vectors,residues,residue,frames)
+            if args.P == 1:
+                mat[:][residue] = angles(vectors,residues,residue,frames,p1_angle)
+            elif args.P == 2:
+                mat[:][residue] = angles(vectors,residues,residue,frames,p2_angle)
             residue_ids.append(sel1[residue].resid)
         mat = mat+mat.T
         np.fill_diagonal(mat, mat.diagonal()/2.0)
-        np.savetxt(f'{args.out}ired_mat.dat',mat,fmt='%9.6f')
+        np.savetxt(f'{args.out}mat_ired_{args.P}.dat',mat,fmt='%9.6f')
         plot_mat(mat,residue_ids)
         S2 = ired(mat)
         result = list(zip(residue_ids,S2))
-        np.savetxt(f'{args.out}order_ired.dat',result,fmt=['%8d','%8.5f'],header='{:7s}{:>8s}'.format('Residue','S2'))
+        np.savetxt(f'{args.out}order_ired_{args.P}.dat',result,fmt=['%8d','%8.5f'],header='{:7s}{:>8s}'.format('Residue','S2'))
 
 if __name__ == '__main__':
     run()
